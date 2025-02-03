eca8b466b848d2a6060e8bd4ea2ab48dd1962a83
Kgo890 <gorekaden501@gmail.com>
2025-02-02 20:37:09 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+is_magician = False
+is_expert = False
+
+if is_magician and is_expert:
+    print("you are a master magician")
+elif is_magician and is_expert == False:
+    print("at least you're getting there")
+elif not is_magician:
+    print("you need magic powers")
+else:
+    print("you are nothing")
